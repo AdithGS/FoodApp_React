b9a3b7a763a632224b7655d511cb6a2e23ef5b13
adithgs <adithgs111@gmail.com>
2024-01-25 22:50:13 +0530

    checkpointw

--- FoodApp/src/components/Body.jsx
+++ FoodApp/src/components/Body.jsx
@@ -2,12 +2,16 @@ import { useEffect, useState } from "react";
 import RestaurantCards from "./RestaurantCards";
 import { DOWN_ARROW_SVG } from "../utils/common";
 import Shimmer from "./Shimmer";
+import { CiSearch } from "react-icons/ci";
+
 // import RestoCardComponent from "./RestoCardComponent";
 const Body = () => {
   // function truncate(str)  {
   //     return str.length > 10 ? str.substring(0,25) + "..." : str;
   // }
   const [restListArray, setRestListArray] = useState([])
+  let [filteredRestArray, setFileterdRestArray] = useState([])
+  const [searchText, setSearchText] = useState("")
   useEffect(() => {
     getData()
   }, [])
@@ -15,17 +19,44 @@ const Body = () => {
   const getData = async () => {
     const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
     const restListJson = await data.json()
-    setRestListArray(restListJson?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    setRestListArray(restListJson?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    console.log(restListArray)
+    setFileterdRestArray(restListJson?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
   }
-  if(restListArray.length === 0){
-    return <Shimmer/>
-  }
-  
-  return (
-    <div className="flex flex-col w-full mx-8">
+
+  return restListArray.length === 0 ? (<Shimmer />) : (
+    <div className=" flex flex-col w-full mx-8">
+      <div className="search">
+        <div className="flex flex-row mx-3 my-3">
+          <label className="relative block w-96 flex flex-row">
+            <span className="sr-only">Search</span>
+            <input className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search for restaurants..." type="text" name="search"
+              value={searchText} onChange={(e) => {
+                setSearchText(e.target.value)
+              }} />
+              <button className="w-48 text-slate-600" onClick={()=>{
+                console.log("Click")
+                filteredRestArray = restListArray.filter((ele) =>{
+                  ele.info.name.toLowerCase().includes(searchText.toLowerCase())
+                })
+              }}> Search</button>
+          </label>
+
+        </div>
+      </div>
+      {/* <div className="filter">
+        <button onClick={()=>{ 
+          console.log("Onclicked")
+          const fiterRest = restListArray.filter((rest)=>{
+          rest.info.avgRating > 4
+        })
+        setRestListArray(fiterRest)
+       }} > Top Rated</button>
+
+      </div> */}
       <div className="flex flex-row space-x-4  flex-wrap">
         {
-          restListArray?.map((rest) => (<RestaurantCards key={rest.info.id} resName={rest.info.name} cuisine={rest.info.cuisines} restImage={rest.info.restImage} />))
+          filteredRestArray?.map((rest) => (<RestaurantCards key={rest.info.id} resName={rest.info.name} cuisine={rest.info.cuisines} restImage={rest.info.restImage} />))
         }
       </div>
     </div>
--- FoodApp/src/components/RestaurantCards.jsx
+++ FoodApp/src/components/RestaurantCards.jsx
@@ -2,21 +2,27 @@ import { REST_IMG } from "../utils/common";
 import { MdStars } from "react-icons/md";
 const RestaurantCards = (props) => {
   return (
-    <div className="flex flex-col border border-gray-300 mx-3 my-3 rounded-lg shadow-md overflow-hidden w-64">
-      <div className="p-4">
+    <div className="flex flex-col transform transition-transform hover:scale-95 border border-gray-300 mx-3 my-3 rounded-lg shadow-md overflow-hidden w-64">
+      <div className="flex justify-center items-center">
         <img
-          className="w-32 h-28 object-cover"
+          className="w-60 h-36  rounded object-cover mx-auto my-2"
           src={REST_IMG}
           // src={props.restImage}
           alt="Restaurant Logo"
         />
       </div>
+
       <div className="p-4 flex flex-col justify-center">
         <h3 className="text-lg font-semibold text-gray-900">{props.resName}</h3>
         <h4 className="text-sm font-light text-gray-600">
-          <span className=" flex flex-row gap-x-2 text-slate-800 font-mono"><MdStars/>4.5. 38 minutes</span>
+          <span className=" flex flex-row gap-x-2 text-slate-800 font-mono">
+            <MdStars />
+            4.5. 38 minutes
+          </span>
         </h4>
-        <h4 className="text-sm font-light text-gray-400">{props.cuisine.join(", ")}</h4>
+        <h4 className="text-sm font-light text-gray-400">
+          {props.cuisine.join(", ")}
+        </h4>
       </div>
     </div>
   );
